b283f00b7b07ce5a136591c0600842e925e9b300
yarkinovweb <yarkinovweb@gmail.com>
2023-12-13 09:16:12 +0500

    updated
    uptadet

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import "./Responsive.css";
 import Translator from "lotin-kirill";
+import Footer from "./Footer";
 
 function App() {
   const translator = new Translator();
@@ -28,29 +29,59 @@ function App() {
   };
 
   return (
-    <div className="container">
-      <h1>
-        Ushbu dastur matiningizni <span>LOTINDAN KIRILGA</span> yoki <b>KIRILDAN LOTINGA</b> o'girib beradi
-      </h1>
+    <>
 
-      <form>
-        <div className="header">
-          <textarea value={originalText} onChange={handleOnChange} className="st" placeholder="Nimadir yozing ..."></textarea>
-          <div className="buttons">
-            <button onClick={toggle} type="button" className="btn btn-primary arrow"><i class="fa-solid fa-arrow-right-arrow-left"></i></button>
-            <button onClick={copyToClipboard} type="button" className="btn btn-danger copy"><i class="fa-regular fa-copy"></i></button>
+      <div className="container">
+        <h1>
+          Ushbu dastur matiningizni <span>LOTINDAN KIRILGA</span> yoki{" "}
+          <b>KIRILDAN LOTINGA</b> o'girib beradi
+        </h1>
+
+        <form>
+          <div className="header">
+            <textarea
+              value={originalText}
+              onChange={handleOnChange}
+              className="st"
+              placeholder="Nimadir yozing ..."
+            ></textarea>
+            <div className="buttons">
+              <button
+                onClick={toggle}
+                type="button"
+                className="btn btn-primary arrow"
+              >
+                <i class="fa-solid fa-arrow-right-arrow-left"></i>
+              </button>
+              <button
+                onClick={copyToClipboard}
+                type="button"
+                className="btn btn-danger copy"
+              >
+                <i class="fa-regular fa-copy"></i>
+              </button>
+            </div>
+            <textarea
+              value={translatedText}
+              className="nd"
+              placeholder="Natija"
+            ></textarea>
           </div>
-          <textarea value={translatedText} className="nd" placeholder="Natija"></textarea>
-        </div>
 
-        <div className="footer">
-          <label className="lotin">{isOriginalTextLatin ? "Lotincha" : "Kirilcha"}</label>
-          <label className="kiril">{isOriginalTextLatin ? "Kirilcha" : "Lotincha"}</label>
-        </div>
+          <div className="footer">
+            <label className="lotin">
+              {isOriginalTextLatin ? "Lotincha" : "Kirilcha"}
+            </label>
+            <label className="kiril">
+              {isOriginalTextLatin ? "Kirilcha" : "Lotincha"}
+            </label>
+          </div>
+        </form>
+        
+      </div>
 
-      </form>
-
-    </div>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer__end">
+      <a target="_blank" href="https://t.me/yarkinovv">Sayt yaratuvchisi bilan bog'lanish</a>
+    </div>
+  );
+};
+
+export default Footer;
